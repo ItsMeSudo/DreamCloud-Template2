15b8dac08b7b647d91191de6a6db7154bd4fc2d9
SUDO <82466939+ItsMeSudo@users.noreply.github.com>
2022-08-29 00:53:48 +0200

    Update to V14

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const dotenv = require('dotenv').config();
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Partials } = require('discord.js');
 const prefix = "&"
 const figlet = require('figlet');
+const colors = require("colors");
 const token = dotenv.parsed.TOKEN;
 const client = new Client({
     intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildBans,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Channel],
 });
 
 figlet('A bot fut, by: DreamCloud!', function (err, data) {
-    console.log(data);
+    console.log(data.blue);
 });
 
 client.on('ready', () => {
     setInterval(()=>{
-        client.user.setActivity(`DreamCloud.hu`, { type: "WATCHING"})
+        client.user.setPresence({
+            activities: [{ name: `DreamCloud.hu`, type: ActivityType.Watching }],
+            status: 'dnd',
+          });
     },30000)
-    client.user.setActivity(`DreamCloud.hu`, { type: "WATCHING"})
+    client.user.setPresence({
+        activities: [{ name: `DreamCloud.hu`, type: ActivityType.Watching }],
+        status: 'dnd',
+    });
     console.log(`Sikeresen bejeletkezett ${client.user.tag} néven.`)
 });
 
@@ -28,4 +39,19 @@ client.on("messageCreate", message => {
     }
 });
 
-client.login(token);
+
+//ANTI CRASH
+if (!token) {
+    console.warn("[CRASH] Token nélkül a bot nem tud elindulni!".red + "\n")
+    return process.exit();
+};
+client.login(token)
+  .catch((err) => {
+    console.error("[CRASH] Nem sikerült kapcsolódni a Discord API-hoz." + "\n");
+    console.error("[CRASH] Discord hiba:" + err);
+    process.exit();
+});
+process.on('unhandledRejection', async (err, promise) => {
+    console.error(`[ANTI-CRASH] Váratlan hiba történt: ${err}`.red);
+    console.error(promise);
+});
